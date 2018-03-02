a2db5af1241846cd4ea1c60b47b691e3d5daa320
gilsonviana <vianawebdev@gmail.com>
2018-03-02 09:06:00 -0500

    about us

--- public/about-us.php
+++ public/about-us.php
@@ -4,8 +4,65 @@
 
         <section class="sec-header">
             <?php include('partials/navbar.php'); ?>
+
+            <div class="container">
+                <div class="content text-center">
+                    <h1 class="text-uppercase">We help you achieve your financial goals</h1>                                                            
+                </div>
+            </div>
+        </section>    
+
+        <section class="sec-about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="content">
+                            <div class="wrapper">
+                                <img src="../src/imgs/logo.png" alt="logo">
+                            </div>
+                            <p class="lead">Quantum Wealth Group was founded by a group of veteran Wall Street financiers with the sole intention of assisting private investors, trusts, institutions and pensions which manage less than $50 billion in assets the opportunity to gain access to a fair and orderly approach of transacting and capturing the benefits of alternative investments. Utilizing a seamless and transparent network of diverse trading and hedging strategies using the utmost modern technology that typically only serves the largest of funds and institutions, Quantum Wealth Group truly gives each investor the same opportunity that the largest of banks have been able to utilize to increase profits and preserve capital from for decades.</p>
+                            <p class="lead">With over 40 years of combined industry experience and billions of dollars in transactional business our team of seasoned veterans can help you achieve your financial goals all while securing your other investments such as stocks, bonds, mutual funds and real estate. With a well-tailored strategy to fit your specific needs let us level the playing field for you and help protect your current assets while we show you risk adjusted returns through alternative investments.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
-    </divmain>
+        <section class="sec-offices">
+            <div class="container">
+                <h1 class="text-center">Global Offices</h1>
+                <div class="line"
+                    data-aos="fade-right"></div>
+
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6">
+                        <div class="wrapper" data-aos="fade-up">
+                            <img src="../src/imgs/1_office.jpg" alt="office">
+                            <h2>New York</h2>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                    <div class="wrapper" data-aos="fade-up" data-aos-delay="100">
+                            <img src="../src/imgs/2_office.jpg" alt="office">
+                            <h2>Danbury</h2>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                    <div class="wrapper" data-aos="fade-up" data-aos-delay="150">
+                            <img src="../src/imgs/3_office.jpg" alt="office">
+                            <h2>Chicago</h2>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                    <div class="wrapper" data-aos="fade-up" data-aos-delay="200">
+                            <img src="../src/imgs/4_office.jpg" alt="office">
+                            <h2>Miami</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div><!-- /about-us -->
 
 <?php include('partials/footer.php'); ?>
--- public/partials/footer.php
+++ public/partials/footer.php
@@ -8,8 +8,7 @@
                                 </a>          
                                 <div>
                                     <h4>Quantum Wealth Group</h4>
-                                    <p>It's the first step in securing your financial future.</p>  
-                                    <p>Phone: 800-985-2510</p>                              
+                                    <p>It's the first step in securing your financial future.</p>                                      
                                     <p>Address: 36 mill plain road suite 403 Danbury CT 06811</p>
                                 </div>                                    
                             </div>
@@ -30,10 +29,22 @@
                                     <li>
                                         <a href="contact.php">Contact</a>
                                     </li>
-                                </ul>
+                                </ul>                                
                             </div>
-                        </div>                    
-                    </div>
+                        </div>                                            
+                        <div class="col-sm-12">
+                            <hr>                            
+                            <p class="text-center">Quantum Wealth Group Inc. | New York | Danbury | Chicago | Miami | San Francisco</p>                            
+                            <div class="text-center">
+                                <a href="quantwealthgroup.com" rel="noopener" taget="_blank">www.quantwealthgroup.com</a> | (800) 985-2510
+                            </div>
+                        </div>
+                    </div>                
+                    <!-- <div class="row">
+                        <div class="col-xs-12">
+                            Quantum Wealth Group Inc. | New York | Danbury | Chicago | Miami | San Francisco
+                        </div>
+                    </div> -->
                 </div>
             </footer>
 
